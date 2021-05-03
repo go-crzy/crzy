1b0d882b0b92c673cbb83150e031c8a8f711530e
gregory <10611760+gregoryguillou@users.noreply.github.com>
2021-05-03 09:22:58 +0200

    add simple test

--- pkg/git_test.go
+++ pkg/git_test.go
@@ -1,1 +1,21 @@
 package pkg
+
+import (
+	"runtime"
+	"testing"
+)
+
+func Test_execCmd(t *testing.T) {
+	switch runtime.GOOS {
+	case "windows":
+		t.Error("this test does not work on windows")
+	default:
+		output, err := execCmd(".", "echo", "-n", "test")
+		if err != nil {
+			t.Error("test fails", err)
+		}
+		if string(output) != "test" {
+			t.Errorf("output should be test, current %q", output)
+		}
+	}
+}
