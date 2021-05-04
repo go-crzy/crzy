f3d52c23e1db7cbb44c66821cfefb5ec094864f7
Namilie <63634646+Namilie@users.noreply.github.com>
2021-05-04 15:15:02 +0200

    test git version windows

--- pkg/git_test.go
+++ pkg/git_test.go
@@ -12,8 +12,8 @@ func Test_execCmd(t *testing.T) {
 		if err != nil {
 			t.Error("test fails", err)
 		}
-		if string(output) != "git version 2.31.1.windows.1\n" {
-			t.Errorf("output should be git version 2.31.1.windows.1, current %q", output)
+		if string(output[0:11]) != "git version" {
+			t.Errorf("output should be git version, current %q", output)
 		}
 		
 		//t.Error("this test does not work on windows")
