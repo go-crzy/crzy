6d7c943d8897503a01b57502f02b8b2a5ca1e849
Namilie <63634646+Namilie@users.noreply.github.com>
2021-05-04 15:05:45 +0200

    Test windows

--- pkg/git_test.go
+++ pkg/git_test.go
@@ -8,7 +8,15 @@ import (
 func Test_execCmd(t *testing.T) {
 	switch runtime.GOOS {
 	case "windows":
-		t.Error("this test does not work on windows")
+		output, err := execCmd(".", "git", "version")
+		if err != nil {
+			t.Error("test fails", err)
+		}
+		if string(output) != "git version 2.31.1.windows.1\n" {
+			t.Errorf("output should be git version 2.31.1.windows.1, current %q", output)
+		}
+		
+		//t.Error("this test does not work on windows")
 	default:
 		output, err := execCmd(".", "echo", "-n", "test")
 		if err != nil {
