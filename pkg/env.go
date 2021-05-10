ea78b48e86e4d788453de715069af9100ca2a1c4
Gregory Guillou <10611760+gregoryguillou@users.noreply.github.com>
2021-05-10 10:07:24 +0200

    Add function to manage Environment Variables (#28)

---
+++ pkg/env.go
@@ -0,0 +1,47 @@
+package pkg
+
+import (
+	"errors"
+	"regexp"
+)
+
+type EnvVar struct {
+	Name  string
+	Value string
+}
+
+var (
+	ErrMissingEnv    = errors.New("missing")
+	ErrDuplicateKeys = errors.New("dupkeys")
+	envPattern       = regexp.MustCompile(`(\$\{[a-zA-Z0-9_]*\})`)
+)
+
+// replaceEnvs replaces variables identified with ${} in param with their
+// values picked from the envs map. If one value is missing, it returns the
+// ErrMissingEnv error.
+func replaceEnvs(param string, envs map[string]string) (string, error) {
+	submatches := envPattern.FindAllStringSubmatch(param, -1)
+	for _, match := range submatches {
+		key := match[0][2 : len(match[0])-1]
+		val, ok := envs[key]
+		if !ok {
+			return "", ErrMissingEnv
+		}
+		envPatternWithKey := regexp.MustCompile(`(\$\{` + key + `\})`)
+		param = envPatternWithKey.ReplaceAllString(param, val)
+	}
+	return param, nil
+}
+
+// groupEnvs returns a map of values built with EnvVar and detect when there
+// is a duplicate key in the list
+func groupEnvs(envs ...EnvVar) (map[string]string, error) {
+	keys := map[string]string{}
+	for _, v := range envs {
+		if _, ok := keys[v.Name]; ok {
+			return map[string]string{}, ErrDuplicateKeys
+		}
+		keys[v.Name] = v.Value
+	}
+	return keys, nil
+}
---
+++ pkg/env_test.go
@@ -0,0 +1,66 @@
+package pkg
+
+import (
+	"testing"
+)
+
+func Test_ReplaceEnv_and_succeed(t *testing.T) {
+	input := []string{`abc-${version}-${x}`, `abc-${version}`, `${version}`}
+	envs := map[string]string{"version": "123", "x": "abc"}
+	expected := []string{`abc-123-abc`, `abc-123`, "123"}
+
+	for k, v := range input {
+		output, err := replaceEnvs(v, envs)
+		if err != nil {
+			t.Error(err, "error replacing key")
+		}
+		if output != expected[k] {
+			t.Errorf("expected value %s, return %s", expected[k], output)
+		}
+	}
+}
+
+func Test_ReplaceEnv_and_failure(t *testing.T) {
+	input := []string{`abc-${version}-${x}`, `abc-${version}`}
+	envs := map[string]string{"x": "abc"}
+
+	for _, v := range input {
+		_, err := replaceEnvs(v, envs)
+		if err != ErrMissingEnv {
+			t.Error("we should fail and we are not")
+		}
+	}
+}
+
+func Test_GroupEnvs_and_succeed(t *testing.T) {
+	input := []EnvVar{{Name: "VERSION", Value: "1.0"}, {Name: "PORT", Value: "8080"}}
+	mapOfEnvs, err := groupEnvs(input...)
+	if err != nil {
+		t.Error("groupEnvs should succeed")
+	}
+	version, ok := mapOfEnvs["VERSION"]
+	if !ok {
+		t.Error("We should have a version")
+	}
+	if version != "1.0" {
+		t.Errorf("version should be 1.0, it is %s", version)
+	}
+	port, ok := mapOfEnvs["PORT"]
+	if !ok {
+		t.Error("We should have a version")
+	}
+	if port != "8080" {
+		t.Errorf("port should be 8080, it is %s", port)
+	}
+}
+
+func Test_GroupEnvs_and_fail(t *testing.T) {
+	input := []EnvVar{{Name: "VERSION", Value: "1.0"}, {Name: "VERSION", Value: "2.0"}}
+	mapOfEnvs, err := groupEnvs(input...)
+	if err != ErrDuplicateKeys {
+		t.Error("groupEnvs should return ErrDuplicateKeys")
+	}
+	if len(mapOfEnvs) != 0 {
+		t.Error("mapOfEnvs should be empty")
+	}
+}
