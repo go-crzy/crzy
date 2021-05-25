a3c889aaac8f4d7de842bc39975b233fdb9a1698
Namilie <63634646+Namilie@users.noreply.github.com>
2021-05-25 17:38:00 +0200

    add test versions details

--- pkg/state_test.go
+++ pkg/state_test.go
@@ -15,7 +15,7 @@ func (m *mockState) listVersions() []byte {
 }
 
 func (m *mockState) listVersionDetails(version string) ([]byte, error) {
-	return []byte(`{"versions": ["123"]}`), nil
+	return []byte(`{"runners": {"deploy": {} }}`), nil
 }
 
 func (m *mockState) addStep(stepEvent) {
