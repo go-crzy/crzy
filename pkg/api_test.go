ca590778fe7edc97519431808feec52bcc3d1080
Namilie <63634646+Namilie@users.noreply.github.com>
2021-05-26 15:10:44 +0200

    add action handler

--- pkg/api_test.go
+++ pkg/api_test.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"bytes"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -82,6 +83,31 @@ func TestApiVersionsDetails(t *testing.T) {
 		t.Errorf("Should not return %v", err)
 	}
 	if string(body) != `{"runners": {"deploy": {} }}` {
+		t.Errorf("Expecting version, get: %s", string(body))
+	}
+}
+
+func TestApiAction(t *testing.T) {
+	handler := &actionHandler{}
+	server := httptest.NewServer(handler)
+	client := server.Client()
+
+	request, _ := http.NewRequest(http.MethodPost, server.URL+"/v0/actions", bytes.NewBufferString(`{"command":"start"}`))
+	response, err := client.Do(request)
+	if err != nil {
+		t.Errorf("Should not return %v", err)
+	}
+	if response.StatusCode != http.StatusOK {
+		t.Errorf(
+			"Status Code should be 200, current: %d",
+			response.StatusCode,
+		)
+	}
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Errorf("Should not return %v", err)
+	}
+	if string(body) != `{"message":"started"}` {
 		t.Errorf("Expecting version, get: %s", string(body))
 	}
 }
